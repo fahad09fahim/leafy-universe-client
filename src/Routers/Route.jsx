e8e43ec7605f63e922b7ab806050c47d4306f027
unknown <anik.fahim07@gmail.com>
2023-09-26 19:15:37 +0600

    banner implemented

--- src/Routers/Route.jsx
+++ src/Routers/Route.jsx
@@ -2,12 +2,17 @@ import {createBrowserRouter,} from "react-router-dom";
 import Main from "../Layout/Main";
 import Login from "../pages/Shared/Login/Login";
 import SignUp from "../pages/Shared/Registration/SignUp";
+import Home from "../pages/Home/Home/Home";
 
   const router = createBrowserRouter([
     {
       path: "/",
       element:<Main/>,
       children:[
+        {
+          path:'/',
+          element:<Home/>
+        },
         {
           path:'login',
           element:<Login/>
---
+++ src/pages/Home/Banner/Banner.jsx
@@ -0,0 +1,18 @@
+
+
+const Banner = () => {
+    return (
+        <div className="hero min-h-screen"  style={{backgroundImage: 'url(https://i.imgur.com/TcW1tPB.png)'}}>
+  <div className="hero-overlay bg-opacity-60"></div>
+  <div className="hero-content text-center text-neutral-content">
+    <div className="max-w-md">
+      <h1 className="mb-5 text-5xl font-bold">Welcome</h1>
+      <p className="mb-5 text-xl">Leafy Universe</p>
+      <p className="mb-5 text-3xl"><span className="text-orange-600 font-bold font-serif">Cultivate</span> your paradise</p>
+    </div>
+  </div>
+</div>
+    );
+};
+
+export default Banner;
